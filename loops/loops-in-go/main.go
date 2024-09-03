56dcd1247e30eefc0cd8e3d82af98dcf4e0b4e8f
INGCapaDev <ingcapadev@gmail.com>
2024-09-02 19:03:29 -0700

    :zap: [CHORE]: While loop alternative in Go

--- loops/loops-in-go/main.go
+++ loops/loops-in-go/main.go
@@ -2,11 +2,10 @@ package main
 
 func bulkSend(numMessages int) float64 {
 	baseCost := 1.0
-	baseAdditionalFee := 0.01
+	additionalFee := 0.01
 	totalCost := 0.0
 	for i := 0; i < numMessages; i++ {
-
-		totalCost += baseCost + (float64(i) * baseAdditionalFee)
+		totalCost += baseCost + (float64(i) * additionalFee)
 	}
 	return totalCost
 }
---
+++ loops/while-loop/go.mod
@@ -0,0 +1,2 @@
+module example.com/loops/while-loop
+go 1.23
